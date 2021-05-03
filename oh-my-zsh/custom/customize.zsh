51497b3866ab624c8a256cd7414d5f67c25a3a34
Barry Hoggard <barry@hoggard.org>
2021-05-03 15:15:49 -0400

    kubernetes-related alias updates

--- oh-my-zsh/custom/customize.zsh
+++ oh-my-zsh/custom/customize.zsh
@@ -1,5 +1,6 @@
 export PATH="/usr/local/sbin:$PATH"
 export PATH="/usr/local/opt/postgresql@12/bin:$PATH"
+export PATH="/usr/local/opt/imagemagick@6/bin:$PATH"
 
 export ANDROID_HOME=$HOME/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/emulator
@@ -14,8 +15,8 @@ alias rub="git diff origin/master --name-only | xargs bundle exec rubocop -a"
 alias gpc="hokusai production run --tty 'rails c production'"
 alias gsc="hokusai staging run --tty 'rails c staging'"
 
-alias pc="hokusai production run --tty 'rails c'"
-alias sc="hokusai staging run --tty 'rails c'"
+alias pc="hokusai production run --tty 'bundle exec rails c'"
+alias sc="hokusai staging run --tty 'bundle exec rails c'"
 
 export HOKUSAI_ALWAYS_VERBOSE=True
 
@@ -24,3 +25,6 @@ export EDITOR=vim
 alias tf=terraform
 
 alias netshoot='kubectl run tmp-shell-$(whoami) --restart=Never --rm -i --tty --image nicolaka/netshoot -- /bin/bash'
+
+alias 'k8s'='aws-iam-authenticator token -i $(kubectl config get-clusters | grep kubernetes-staging) -r arn:aws:iam::585031190124:role/KubernetesDev | jq -r .status.token | pbcopy'
+alias 'k8p'='aws-iam-authenticator token -i $(kubectl config get-clusters | grep kubernetes-production) -r arn:aws:iam::585031190124:role/KubernetesDev | jq -r .status.token | pbcopy'
